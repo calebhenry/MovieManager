6b81f9df8f4f744413e5292db109745781c554c8
Caleb Henry <calebhenry2003@gmail.com>
2024-10-17 01:19:21 -0400

    Wierd cache update

--- MovieManager.Server/obj/Debug/net8.0/MovieManager.Server.AssemblyInfo.cs
+++ MovieManager.Server/obj/Debug/net8.0/MovieManager.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieManager.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d843355f2a3430aa960d48801a478a052dca6676")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+86b4d084b6a7ca86614c8fd9cdf9cb3863a968cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieManager.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieManager.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
