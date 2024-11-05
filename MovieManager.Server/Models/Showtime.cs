dbfe9f75c1dfa5163634c4c773c25e77cc9573fa
Carolina Turner <carolinanturner@gmail.com>
2024-11-05 13:13:28 -0500

    updating payment method

--- MovieManager.Server/Models/Showtime.cs
+++ MovieManager.Server/Models/Showtime.cs
@@ -5,6 +5,7 @@ namespace MovieManager.Server.Models
     public class Showtime
     {
         public int NumAvailible { get; set; }
-        public DateTime Time { get; set;}
+        public DateTime Showtime { get; set;}
+        public int MovieId { get; set;}
     }
 }
