f17a73824a0f2e20afcc3b2ef444934b11eb0fed
Dylan-B-Johnson <dmaxbear123@gmail.com>
2024-11-12 22:05:19 -0500

    ready for pr

---
+++ MovieManager.Server/MoviesContext.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using MovieManager.Server.Models;
+
+namespace MovieManager.Server;
+
+public partial class MoviesContext : DbContext
+{
+    public MoviesContext()
+    {
+    }
+
+    public MoviesContext(DbContextOptions<MoviesContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Cart> Carts { get; set; }
+
+    public virtual DbSet<CartItem> CartItems { get; set; }
+
+    public virtual DbSet<Movie> Movies { get; set; }
+
+    public virtual DbSet<Review> Reviews { get; set; }
+
+    public virtual DbSet<Ticket> Tickets { get; set; }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=ConnectionStrings:movieDb");
+
+}
--- MovieManager.Server/Services/IMovieService.cs
+++ MovieManager.Server/Services/IMovieService.cs
@@ -8,16 +8,16 @@ namespace MovieManager.Server.Services
         void AddMovie(Movie movie);
         bool AddTicketToCart(int cartId, int ticketId, int quantity);
         User AddUser(User user);
-        List<Movie> GetMovies();
-        Movie GetMovieById(int id);
-        User? GetUser(string username, string password);
-        User UpdateUser(UpdatedUser updatedUser);
-        bool RemoveMovie(Movie movie);
-        Cart? RemoveTicketFromCart(int ticketId, int cartId);
-        void RemoveTicket(Ticket ticket);
-        void RemoveUser(User user);
-        void ProcessPayment(int cartId, string cardNumber, string exp, string cardholderName, string cvc);
-        IEnumerable<Ticket> GetTickets(int movieId);
         Cart GetCart(int? cartId);
+        Movie? GetMovieById(int id);
+        List<Movie> GetMovies();
+        IEnumerable<Ticket> GetTickets(int movieId);
+        User? GetUser(string username, string password);
+        void ProcessPayment(int cartId, string cardNumber, string exp, string cardholderName, string cvc);
+        bool RemoveMovie(Movie movie);
+        void RemoveTicket(Ticket ticket);
+        Cart? RemoveTicketFromCart(int ticketId, int cartId);
+        void RemoveUser(User user);
+        User UpdateUser(UpdatedUser updatedUser);
     }
 }
